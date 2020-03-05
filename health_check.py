05c243e5e36769e1abe2ff300fb4ef742b6ad856
Prayag Upd <prayag.upd@gmail.com>
2020-03-04 22:25:49 -0800

    add test python code to execute parallel requests

---
+++ health_check.py
@@ -0,0 +1,26 @@
+import concurrent.futures
+import urllib.request
+
+URLS = ['http://www.foxnews.com/',
+        'http://www.cnn.com/',
+        'http://europe.wsj.com/',
+        'http://www.bbc.co.uk/',
+        'http://some-made-up-domain.com/']
+
+# Retrieve a single page and report the URL and contents
+def check_health(url, timeout):
+    with urllib.request.urlopen(url, timeout=timeout) as conn:
+        return conn.read()
+
+# We can use a with statement to ensure threads are cleaned up promptly
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    # Start the load operations and mark each future with its URL
+    tasks = {executor.submit(check_health, url, 60): url for url in URLS}
+    for task in concurrent.futures.as_completed(tasks):
+        url = tasks[task]
+        try:
+            data = task.result()
+        except Exception as exc:
+            print('%r generated an exception: %s' % (url, exc))
+        else:
+            print('%r page is %d bytes' % (url, len(data)))
