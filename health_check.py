a584a994dfed6b75c8692503a475dfc47aef9d68
Prayag Upd <prayag.upd@gmail.com>
2020-03-05 21:13:30 -0800

    define a method

--- health_check.py
+++ health_check.py
@@ -6,26 +6,30 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+import time
+
 URLS = ['https://api.github.com',
         'https://twitter.com',
         'https://api.facebook.com',
         'https://expedia.com',
         'http://some-made-up-domain.com']
 
-# Retrieve a single page and report the URL and contents
 def check_health(url, timeout):
     with urllib.request.urlopen(url, context=ctx, timeout=timeout) as conn:
         return conn.getcode()
 
-# We can use a with statement to ensure threads are cleaned up promptly
-with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-    # Start the load operations and mark each future with its URL
-    tasks = {executor.submit(check_health, url, 60): url for url in URLS}
-    for task in concurrent.futures.as_completed(tasks):
-        url = tasks[task]
-        try:
-            data = task.result()
-        except Exception as exc:
-            print('%r : %s' % (url, exc))
-        else:
-            print('%r : %s' % (url, data))
+def check_healths():
+    # We can use a with statement to ensure threads are cleaned up promptly
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        # Start the load operations and mark each future with its URL
+        tasks = {executor.submit(check_health, url, 60): url for url in URLS}
+        for task in concurrent.futures.as_completed(tasks):
+            url = tasks[task]
+            try:
+                data = task.result()
+            except Exception as exc:
+                print('%r : %s' % (url, exc))
+            else:
+                print('%r : %s' % (url, data))
+
+check_healths()
