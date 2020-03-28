863c9d8358a25cf83b1115a582a4b550efdfeb1c
Prayag Upd <prayag.upd@gmail.com>
2020-03-28 12:58:06 -0700

    add kubernetes configs based health

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 swasthya
 --------
 
+![](swasthya.png)
+
 ```
 brew install helm
 
-
+eval $(minikube docker-env) ## in order be in minikube context/ makes containers avail
 docker build -t swasthya-server:1.0 .
 kubectl create namespace dev
 kubectl apply -f k8s-healthcheck-deployment.yaml --namespace dev
 ```
+
+
+Also see
+--------
+
+- https://github.com/prayagupd/eccount-rest/tree/master/devops
--- health_check.py
+++ health_check.py
@@ -6,29 +6,34 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+import os
 import time
 import sys
 import configparser
 
 configParser = configparser.RawConfigParser()
 configParser.read('application.ini')
-server1 = configParser.get('dev', 'server1')
-server2 = configParser.get('dev', 'server2')
-server3 = configParser.get('dev', 'server3')
-server4 = configParser.get('dev', 'server4')
-server5 = configParser.get('dev', 'server5')
+#server1 = configParser.get('dev', 'server1')
+#server2 = configParser.get('dev', 'server2')
+#server3 = configParser.get('dev', 'server3')
+#server4 = configParser.get('dev', 'server4')
+#server5 = configParser.get('dev', 'server5')
 
-URLS = [server1, server2, server3, server4, server5]
+#URLS_FROM_INI = [server1, server2, server3, server4, server5]
+
+print("loading urls to check health")
+URLS = list(map(lambda url: url.strip(), os.environ['SERVERS'].split(",")))
 
 def check_health(url, timeout):
     with urllib.request.urlopen(url, context=ctx, timeout=timeout) as conn:
         return conn.getcode()
 
 def check_healths():
+    print("checking health for servers")
     # We can use a with statement to ensure threads are cleaned up promptly
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers = len(URLS)) as executor:
         # Start the healthcheck operations and mark each future with its URL
-        sys.stdout.write('starting health check\n')
+        sys.stdout.write("starting health check for " + str(len(URLS)) + " services.\n")
         tasks = {executor.submit(check_health, url, 60): url for url in URLS}
         for task in concurrent.futures.as_completed(tasks):
             url = tasks[task]
